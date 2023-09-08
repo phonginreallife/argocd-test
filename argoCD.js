f2a64c76754e870a5be84945d73fed713c858b19
phonginreallife <112053396+phonginreallife@users.noreply.github.com>
2023-09-08 09:34:26 +0700

    adding codes

---
+++ argoCD.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // req.body
+
+app.get("/private-key", async (req, res) => {
+    try {
+        const privateKey = process.env.PRIVATE_KEY;
+        if (!privateKey) {
+            return res.status(404).json({ error: "Private key not found" });
+        }
+        
+        res.json({ privateKey });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.listen(8088, () => {
+    console.log("Server has started on port 8088");
+});
